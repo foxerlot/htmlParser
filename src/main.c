e8077bebc72f55a37847c25c55c29841d4819e60
foxerlot <95938153+foxerlot@users.noreply.github.com>
2026-02-20 17:14:12 -0800

    figuring out the actual parsing

--- src/main.c
+++ src/main.c
@@ -1,8 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
-// #include <string.h>
+#include <string.h>
 #include "utilities.h"
-// #include "parse-util.h"
 
 /* global variables */
 const char *usage = "USAGE: htmlParser [options] <input.hml>...";
@@ -17,22 +16,30 @@ int main(int argc, char **argv)
 
     char *line = NULL;
     size_t length = 0;
+    ssize_t read;
 
-    while (getline(&line, &length, contents) != -1)
-    {
-        for (int i = 0; i < (int) length; i++)
-        {
-            char *tag = NULL;
-            int k = 1;
-            if (line[i++] == '<')
-            {
-                for (int j = 0; line[j] != '>'; j++)
-                {
-                    tag = realloc(tag, k++ * sizeof(char));
-                    tag[k] = line[j];
+    while ((read = getline(&line, &length, contents)) != -1) {
+        for (int i = 0; i < read; i++) {
+            if (line[i] == '<') {
+                int j = i + 1;
+
+                while (j < read && line[j] != '>')
+                    j++;
+
+                if (j < read) {
+                    int tag_len = j - i - 1;
+
+                    char *tag = malloc(tag_len + 1);
+                    if (!tag) DIE("Failed to allocate memory");
+
+                    memcpy(tag, &line[i + 1], tag_len);
+                    tag[tag_len] = '\0';
+
+                    printf("Tag: %s\n", tag);
+                    free(tag);
+                    i = j;
                 }
             }
-            printf("%s\n", tag);
         }
     }
 
--- src/parse-util.h
+++ src/parse-util.h
@@ -1,9 +1,6 @@
 #ifndef PARSE_UTIL_H
 #define PARSE_UTIL_H
 
-#include <stdio.h>
-
 char tag_type(char*);
-int close(char*, FILE*, char*); /* Look for closing tag (assuming that the tag needs closing,) and create it if not found */
 
 #endif
